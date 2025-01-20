8b70adbbeb6cbaa4af9546641c59dd4bfa6539c6
Kristen Mckenzie <kristenmckenzie214@gmail.com>
2025-01-19 23:09:43 -0600

    added band cards

--- bands.js
+++ bands.js
@@ -1,3 +1,34 @@
 "use strict";
 
+const bands = [
+  { name: "The Beatles", genre: "Rock", formedYear: 1960, active: false },
+  {
+    name: "Coldplay",
+    genre: "Alternative Rock",
+    formedYear: 1996,
+    active: true,
+  },
+  { name: "Linkin Park", genre: "Nu Metal", formedYear: 1996, active: false },
+  { name: "BTS", genre: "K-Pop", formedYear: 2013, active: true },
+];
 
+function renderBands(bands) {
+  const container = document.getElementById("bandsContainer");
+
+  bands.forEach((band) => {
+    const bandCard = `
+   <div class="card" style="width: 18rem;">
+  <div class="card-header">
+    ${band.name}
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${band.genre}</li>
+    <li class="list-group-item">${band.formedYear}</li>
+    <li class="list-group-item">${band.active ? "Active" : "Inactive"}</li>
+  </ul>
+</div>`;
+    container.innerHTML += bandCard;
+  });
+}
+
+renderBands(bands);
